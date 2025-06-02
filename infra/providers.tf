d9e6fd5667e31a998574cad1e7f43380d3c011af
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-06-02 17:15:49 +0200

    Moved providers to a separate .tf file

---
+++ infra/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.28.0"
+    }
+  }
+  backend "azurerm" {
+    use_oidc             = true
+    use_azuread_auth     = true
+    storage_account_name = "tfstatefonteyn"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
