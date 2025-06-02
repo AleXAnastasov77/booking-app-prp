e07527b1704ab7ecac6fbc7937058d51fbf1f54a
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-06-02 17:23:19 +0200

    Changed role to Officer so secrets can be added

--- infra/main.tf
+++ infra/main.tf
@@ -122,7 +122,7 @@ resource "azurerm_key_vault" "booking_keyvault" {
 }
 resource "azurerm_role_assignment" "terraform_keyvault_access" {
   principal_id         = data.azurerm_client_config.current.object_id
-  role_definition_name = "Key Vault Secrets User"
+  role_definition_name = "Key Vault Secrets Officer"
   scope                = azurerm_key_vault.booking_keyvault.id
 }
 resource "azurerm_key_vault_secret" "mysql_username" {
