c978c281c690e7cf0bac20a5c18fc1259c0a39b4
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-06-03 13:26:27 +0200

    switch to westeu

--- infra/main.tf
+++ infra/main.tf
@@ -6,18 +6,23 @@ resource "azurerm_resource_group" "booking_rg" {
   location = var.location
   tags     = var.tags
 }
+resource "azurerm_resource_group" "platform_rg" {
+  name     = var.resource_group_name_platform
+  location = var.location
+  tags     = var.tags
+}
 
 # Hub Vnet and subnets
 resource "azurerm_virtual_network" "booking_hub_vnet" {
   name                = var.hub_vnet_name
   location            = azurerm_resource_group.booking_rg.location
-  resource_group_name = var.resource_group_name_platform
+  resource_group_name = azurerm_resource_group.platform_rg.name
   address_space       = ["10.0.0.0/16"]
   tags                = var.tags
 }
 resource "azurerm_subnet" "db_subnet" {
   name                 = "subnet-db"
-  resource_group_name  = var.resource_group_name_platform
+  resource_group_name  = azurerm_resource_group.platform_rg.name
   virtual_network_name = azurerm_virtual_network.booking_hub_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
   #default_outbound_access_enabled = false
@@ -30,7 +35,7 @@ resource "azurerm_subnet" "db_subnet" {
 }
 resource "azurerm_subnet" "keyvault_subnet" {
   name                 = "subnet-keyvault"
-  resource_group_name  = var.resource_group_name_platform
+  resource_group_name  = azurerm_resource_group.platform_rg.name
   virtual_network_name = azurerm_virtual_network.booking_hub_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 
@@ -62,7 +67,7 @@ resource "azurerm_subnet" "containerapps_subnet" {
 # Connecting the hub and spoke VNets
 resource "azurerm_virtual_network_peering" "hub_to_spoke" {
   name                      = "hub-to-spoke"
-  resource_group_name       = var.resource_group_name_platform
+  resource_group_name       = azurerm_resource_group.platform_rg.name
   virtual_network_name      = azurerm_virtual_network.booking_hub_vnet.name
   remote_virtual_network_id = azurerm_virtual_network.booking_spoke_vnet.id
 }
@@ -77,34 +82,34 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
 #Private DNS Zone for the database
 resource "azurerm_private_dns_zone" "mysql_private_dns_zone" {
   name                = "privatelink.mysql.database.azure.com"
-  resource_group_name = var.resource_group_name_platform
+  resource_group_name = azurerm_resource_group.platform_rg.name
 }
 resource "azurerm_private_dns_zone" "keyvault_private_dns_zone" {
   name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = var.resource_group_name_platform
+  resource_group_name = azurerm_resource_group.platform_rg.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link001" {
   name                  = "dnslink-northeu-001"
-  resource_group_name   = var.resource_group_name_platform
+  resource_group_name   = azurerm_resource_group.platform_rg.name
   private_dns_zone_name = azurerm_private_dns_zone.mysql_private_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.booking_hub_vnet.id
 }
 resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link002" {
   name                  = "dnslink-northeu-002"
-  resource_group_name   = var.resource_group_name_platform
+  resource_group_name   = azurerm_resource_group.platform_rg.name
   private_dns_zone_name = azurerm_private_dns_zone.mysql_private_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.booking_spoke_vnet.id
 }
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_dns_link001" {
   name                  = "dnslink-northeu-003"
-  resource_group_name   = var.resource_group_name_platform
+  resource_group_name   = azurerm_resource_group.platform_rg.name
   private_dns_zone_name = azurerm_private_dns_zone.keyvault_private_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.booking_hub_vnet.id
 }
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_dns_link002" {
   name                  = "dnslink-northeu-004"
-  resource_group_name   = var.resource_group_name_platform
+  resource_group_name   = azurerm_resource_group.platform_rg.name
   private_dns_zone_name = azurerm_private_dns_zone.keyvault_private_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.booking_spoke_vnet.id
 }
@@ -112,7 +117,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_dns_link002" 
 resource "azurerm_key_vault" "booking_keyvault" {
   name                       = var.keyvault_name
   location                   = var.location
-  resource_group_name        = var.resource_group_name_platform
+  resource_group_name        = azurerm_resource_group.platform_rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 10
@@ -137,7 +142,7 @@ resource "azurerm_key_vault_secret" "mysql_password" {
 }
 resource "azurerm_mysql_flexible_server" "booking_db" {
   name                         = var.mysqldb_name
-  resource_group_name          = var.resource_group_name_platform
+  resource_group_name          = azurerm_resource_group.platform_rg.name
   location                     = var.location
   administrator_login          = azurerm_key_vault_secret.mysql_username.value
   administrator_password       = azurerm_key_vault_secret.mysql_password.value
