e174e02559d1b1a1f699ff235fa2027dbf13dfe9
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-06-06 14:00:12 +0200

    test app deployment

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -9,13 +9,13 @@ API_KEY=os.getenv("API_KEY")
 CONFIG = configparser.ConfigParser()
 CONFIG.read("config.ini")
 
-CONFIG["api"]["url"] = os.getenv("API_URL")
 
 def get_headers():
     headers = {
         "X-API-KEY": os.getenv("API_KEY")
     }
     return headers
+
 def get_secret_key():
     secret_key = os.getenv("SECRET_KEY")
     return secret_key
